8d4e2ef0a349ae414dd3c95418a5926202c33950
DaniloDaniel <choopper_14_@hotmail.com>
2018-08-02 00:25:41 -0400

    Initial migrations y cambio de connection string en .DATA y .WEB

---
+++ Munay/Munay.DATA/Migrations/201808020422149_initial-migration.vb
@@ -0,0 +1,140 @@
+Imports System
+Imports System.Data.Entity.Migrations
+Imports Microsoft.VisualBasic
+
+Namespace Migrations
+    Public Partial Class initialmigration
+        Inherits DbMigration
+    
+        Public Overrides Sub Up()
+            CreateTable(
+                "dbo.Articulo",
+                Function(c) New With
+                    {
+                        .ID = c.String(nullable := False, maxLength := 128),
+                        .Nombre = c.String(),
+                        .Foto = c.Binary(),
+                        .Precio = c.Int(nullable := False),
+                        .Descripcion = c.String(),
+                        .Categoria_ID = c.Int()
+                    }) _
+                .PrimaryKey(Function(t) t.ID) _
+                .ForeignKey("dbo.Categoria", Function(t) t.Categoria_ID) _
+                .Index(Function(t) t.Categoria_ID)
+            
+            CreateTable(
+                "dbo.Categoria",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Nombre = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.DetallePedido",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .FechaRegistro = c.DateTime(nullable := False),
+                        .Cantidad = c.Int(nullable := False),
+                        .Subtotal = c.Int(nullable := False),
+                        .Articulo_ID = c.String(maxLength := 128),
+                        .Pedido_ID = c.Int()
+                    }) _
+                .PrimaryKey(Function(t) t.ID) _
+                .ForeignKey("dbo.Articulo", Function(t) t.Articulo_ID) _
+                .ForeignKey("dbo.Pedido", Function(t) t.Pedido_ID) _
+                .Index(Function(t) t.Articulo_ID) _
+                .Index(Function(t) t.Pedido_ID)
+            
+            CreateTable(
+                "dbo.Pedido",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Total = c.Int(nullable := False),
+                        .Cliente = c.String(),
+                        .Email = c.String(),
+                        .Mensaje = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.Nosotro",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Foto = c.Binary(),
+                        .Mision = c.String(),
+                        .Vision = c.String(),
+                        .Descripcion = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.Nota",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Titulo = c.String(),
+                        .Descripcion = c.String(),
+                        .Foto = c.Binary(),
+                        .Enlace = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.Redsocial",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Foto = c.Binary(),
+                        .Fecha = c.DateTime(nullable := False)
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.Token",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Run = c.String(),
+                        .TokenActual = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+            CreateTable(
+                "dbo.Usuario",
+                Function(c) New With
+                    {
+                        .ID = c.Int(nullable := False, identity := True),
+                        .Nombre = c.String(),
+                        .Apellido = c.String(),
+                        .Email = c.String(),
+                        .Run = c.String(),
+                        .Password = c.String()
+                    }) _
+                .PrimaryKey(Function(t) t.ID)
+            
+        End Sub
+        
+        Public Overrides Sub Down()
+            DropForeignKey("dbo.DetallePedido", "Pedido_ID", "dbo.Pedido")
+            DropForeignKey("dbo.DetallePedido", "Articulo_ID", "dbo.Articulo")
+            DropForeignKey("dbo.Articulo", "Categoria_ID", "dbo.Categoria")
+            DropIndex("dbo.DetallePedido", New String() { "Pedido_ID" })
+            DropIndex("dbo.DetallePedido", New String() { "Articulo_ID" })
+            DropIndex("dbo.Articulo", New String() { "Categoria_ID" })
+            DropTable("dbo.Usuario")
+            DropTable("dbo.Token")
+            DropTable("dbo.Redsocial")
+            DropTable("dbo.Nota")
+            DropTable("dbo.Nosotro")
+            DropTable("dbo.Pedido")
+            DropTable("dbo.DetallePedido")
+            DropTable("dbo.Categoria")
+            DropTable("dbo.Articulo")
+        End Sub
+    End Class
+End Namespace
---
+++ Munay/Munay.DATA/MunayDBContext.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+Imports System.Data.Entity
+Imports System.Data.Entity.ModelConfiguration.Conventions
+Imports System.Linq
+
+Public Class MunayDBContext
+    Inherits DbContext
+
+    ' El contexto se ha configurado para usar una cadena de conexión 'MunayDBContext' del archivo 
+    ' de configuración de la aplicación (App.config o Web.config). De forma predeterminada, 
+    ' esta cadena de conexión tiene como destino la base de datos 'Munay.DATA.MunayDBContext' de la instancia LocalDb. 
+    ' 
+    ' Si desea tener como destino una base de datos y/o un proveedor de base de datos diferente, 
+    ' modifique la cadena de conexión 'MunayDBContext'  en el archivo de configuración de la aplicación.
+    Public Sub New()
+        MyBase.New("name=MunayDBContext")
+    End Sub
+
+    ' Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
+    ' sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+    ' Public Overridable Property MyEntities() As DbSet(Of MyEntity)
+    Public Overridable Property Articulos As DbSet(Of Articulo)
+    Public Overridable Property Categorias As DbSet(Of Categoria)
+    Public Overridable Property DetallePedidos As DbSet(Of DetallePedido)
+    Public Overridable Property Nosotros As DbSet(Of Nosotro)
+    Public Overridable Property Notas As DbSet(Of Nota)
+    Public Overridable Property Pedidos As DbSet(Of Pedido)
+    Public Overridable Property Redsociales As DbSet(Of Redsocial)
+    Public Overridable Property Usuarios As DbSet(Of Usuario)
+    Public Overridable Property Token As DbSet(Of Token)
+
+    Protected Overrides Sub OnModelCreating(modelBuilder As DbModelBuilder)
+        MyBase.OnModelCreating(modelBuilder)
+        modelBuilder.Conventions.Remove(Of PluralizingTableNameConvention)()
+        modelBuilder.Conventions.Remove(Of OneToManyCascadeDeleteConvention)()
+        modelBuilder.Conventions.Remove(Of ManyToManyCascadeDeleteConvention)()
+    End Sub
+End Class
+
+'Public Class MyEntity
+'    Public Property Id() As Int32
+'    Public Property Name() As String
+'End Class
