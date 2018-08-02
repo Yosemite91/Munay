b5c2d64a97e462daf99b1ece2e953e8f3adf3c74
Antonio Labraña <antonio@git.uta.com>
2018-08-02 01:06:41 -0400

    Se crea vista principal

---
+++ Munay/Munay.WEB/Scripts/app/Home/home.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+/// <reference path="../../typings/knockout/knockout.d.ts" />
+/// <reference path="../../typings/devextreme/devextreme.d.ts" />
+
+namespace Home {
+    export class home {
+        public noticias: KnockoutObservable<any> = ko.observable<any>();
+
+        public listOptions: DevExpress.ui.dxListOptions = {
+            dataSource: this.noticias,
+            height: "100%",
+            indicateLoading: true,
+            menuMode: "content",
+            nextButtonText: "Más",
+            noDataText: "Sin datos por mostrar",
+            pageLoadingText: "Cargando...",
+            refreshingText: "Recargando...",
+            onItemClick: (e) => {
+                var datoItem = e.itemData;
+                window.location.assign(App.appRoot + 'Publicacion/DetallePublicacion?id=' + datoItem.id);
+            }
+        };
+
+        constructor() {
+            
+        }
+    }
+}
---
+++ Munay/Munay.WEB/Scripts/app/app.ts
@@ -0,0 +1,56 @@
+﻿/// <reference path='Utils.ts' />
+
+namespace App {
+    'use strict'; 
+    export var appRoot: string;
+    export var apiRoot: string;
+    //MODIFICAR PARA LOGIN
+    export var appPrefix: string = 'GPC';        
+    export var esAdministrador: string = localStorage.getItem(App.appPrefix + 'login.esAdministrador');
+    
+    $.ajaxSetup({
+        headers: GetAutorizationHeaders(),
+        error: (jqXHR: any, textStatus: any, errorThrown: any): void => {
+            Utils.getErrores('', jqXHR, textStatus, errorThrown);
+        }
+    });
+
+    function GetAutorizationHeaders(): { [key: string]: any; } {
+        let token: string = localStorage.getItem(appPrefix + 'login.token');
+        if (token) {
+            return { 'Authorization': 'Basic ' + token };
+        } else {
+            return { 'Authorization': 'Anonymous' };
+        }
+    }
+
+    export function goToLogin(): any {                      
+        localStorage.removeItem(appPrefix + 'login.esAdministrador');        
+        localStorage.removeItem(appPrefix + 'login.token');
+        let urlLogin: string = 'http://' + window.location.host + App.apiRoot.replace('/api/', '/');
+        window.location.href = urlLogin;
+        return false;
+    };
+      
+    export function anioMinimo(): any {
+        var actual = new Date(); 
+        var anioMinimo = actual.getFullYear() - 18;        
+        return anioMinimo;
+    };
+   
+    export function alertaFormulario(anyObj: any): boolean {
+        var i: number;        
+        var error: boolean = true;
+      
+        for (i = 0; i < Object.keys(anyObj).length; i++) {                
+            if (anyObj[Object.keys(anyObj)[i]] === null || anyObj[Object.keys(anyObj)[i]] === '' || anyObj[Object.keys(anyObj)[i]] === 0 ) {
+                let name:any = Object.keys(anyObj)[i];
+                DevExpress.ui.notify('Ingrese : ' + name, 'error', 3000);
+                error = false;
+                return error;
+            }
+        }               
+
+        return true;
+    }
+}
